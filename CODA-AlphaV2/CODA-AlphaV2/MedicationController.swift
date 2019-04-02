75295e1f5b5c2a5f4cd66b89911ea81bec829ee5
MrDJEnz <djenzmann@gmail.com>
2019-04-02 19:53:20 -0400

    fixing layout issues

--- CODA-AlphaV2/CODA-AlphaV2/MedicationController.swift
+++ CODA-AlphaV2/CODA-AlphaV2/MedicationController.swift
@@ -12,12 +12,63 @@ class MedicationController: UIViewController {
 
     @IBOutlet weak var userLbl: UILabel!
     
+    @IBDesignable class DesignableView: UIView
+    {
+        @IBInspectable var gradientColor1: UIColor = UIColor.white {
+            didSet{
+                self.setGradient()
+            }
+        }
+
+        @IBInspectable var gradientColor2: UIColor = UIColor.white {
+            didSet{
+                self.setGradient()
+            }
+        }
+
+        @IBInspectable var gradientStartPoint: CGPoint = .zero {
+            didSet{
+                self.setGradient()
+            }
+        }
+
+        @IBInspectable var gradientEndPoint: CGPoint = CGPoint(x: 0, y: 1) {
+            didSet{
+                self.setGradient()
+            }
+        }
+
+        private func setGradient()
+        {
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+            gradientLayer.startPoint = self.gradientStartPoint
+            gradientLayer.endPoint = self.gradientEndPoint
+            gradientLayer.frame = self.bounds
+            if let topLayer = self.layer.sublayers?.first, topLayer is CAGradientLayer
+            {
+                topLayer.removeFromSuperlayer()
+            }
+            self.layer.addSublayer(gradientLayer)
+        }
+    }
+    
+    
     var finalUsername = ""
     var passback = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         userLbl.text = finalUsername
         finalUsername = String(finalUsername.dropFirst(9))
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.purple.cgColor, UIColor.yellow.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = view.bounds
+        view.layer.addSublayer(gradientLayer)
+        
+        
 //        let range1 = finalUsername.characters.index(finalUsername.startIndex, offsetBy: 9)..<finalUsername.endIndex
 //        finalUsername = String(finalUsername[range1])
         // Do any additional setup after loading the view.
