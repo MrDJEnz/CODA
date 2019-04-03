564c962261a3e62d0b23229528e2b26af537edd0
MrDJEnz <djenzmann@gmail.com>
2019-04-02 21:26:49 -0400

    pdf scraper...

--- CODA-AlphaV2/CODA-AlphaV2/DischargeController.swift
+++ CODA-AlphaV2/CODA-AlphaV2/DischargeController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PDFKit
 
 class DischargeController: UIViewController {
     
@@ -63,7 +64,7 @@ class DischargeController: UIViewController {
         userLbl.text = finalUsername
         finalUsername = String(finalUsername.dropFirst(9))
         
-        
+
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [UIColor.blue.cgColor, UIColor.cyan]
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
@@ -72,6 +73,34 @@ class DischargeController: UIViewController {
         //view.layer.addSublayer(gradientLayer)
         self.view.layer.insertSublayer(gradientLayer, at: 1)
         
+        
+//        let pdfView = PDFView()
+//
+//        pdfView.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(pdfView)
+//
+//        pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+//        pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+//        pdfView.topAnchor.constraint(equalTo: view.viewWithTag(3)!.topAnchor).isActive = true
+//        pdfView.bottomAnchor.constraint(equalTo: view.viewWithTag(4)!.bottomAnchor).isActive = true
+//
+//        guard let path = Bundle.main.url(forResource: "lorem-ipsum", withExtension: "pdf") else { return }
+//
+//        if let document = PDFDocument(url: path) {
+//            pdfView.document = document
+//        }
+
+        // Add PDFView to view controller.
+        let pdfView = PDFView(frame: self.view.bounds)
+        pdfView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(pdfView)
+        
+        // Fit content in PDFView.
+        pdfView.autoScales = true
+        
+        // Load Sample.pdf file from app bundle.
+        let fileURL = Bundle.main.url(forResource: "lorem-ipsum", withExtension: "pdf")
+        pdfView.document = PDFDocument(url: fileURL!)
         
         //        let range1 = finalUsername.characters.index(finalUsername.startIndex, offsetBy: 9)..<finalUsername.endIndex
         //        finalUsername = String(finalUsername[range1])
