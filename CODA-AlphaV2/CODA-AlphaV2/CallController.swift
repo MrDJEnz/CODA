f15e3654f1c5d39c77d04fb5da99c3af221f9a85
MrDJEnz <djenzmann@gmail.com>
2019-04-02 20:55:32 -0400

    color gradient...
    need to adjust colors to look nice

--- CODA-AlphaV2/CODA-AlphaV2/CallController.swift
+++ CODA-AlphaV2/CODA-AlphaV2/CallController.swift
@@ -12,12 +12,67 @@ class CallController: UIViewController {
     
     //@IBOutlet weak var userLbl: UILabel!
     @IBOutlet weak var userLbl: UILabel!
+    
+    
+    @IBDesignable class DesignableView: UIView
+    {
+        @IBInspectable var gradientColor1: UIColor = UIColor.white {
+            didSet{
+                self.setGradient()
+            }
+        }
+        
+        @IBInspectable var gradientColor2: UIColor = UIColor.white {
+            didSet{
+                self.setGradient()
+            }
+        }
+        
+        @IBInspectable var gradientStartPoint: CGPoint = .zero {
+            didSet{
+                self.setGradient()
+            }
+        }
+        
+        @IBInspectable var gradientEndPoint: CGPoint = CGPoint(x: 0, y: 1) {
+            didSet{
+                self.setGradient()
+            }
+        }
+        
+        private func setGradient()
+        {
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+            gradientLayer.startPoint = self.gradientStartPoint
+            gradientLayer.endPoint = self.gradientEndPoint
+            gradientLayer.frame = self.bounds
+            if let topLayer = self.layer.sublayers?.first, topLayer is CAGradientLayer
+            {
+                topLayer.removeFromSuperlayer()
+            }
+            self.layer.addSublayer(gradientLayer)
+        }
+    }
+    
+    
     var finalUsername = ""
     var passback = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         userLbl.text = finalUsername
         finalUsername = String(finalUsername.dropFirst(9))
+        
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.cyan]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = view.bounds
+        //view.layer.addSublayer(gradientLayer)
+        self.view.layer.insertSublayer(gradientLayer, at: 1)
+        
+        
         //        let range1 = finalUsername.characters.index(finalUsername.startIndex, offsetBy: 9)..<finalUsername.endIndex
         //        finalUsername = String(finalUsername[range1])
         // Do any additional setup after loading the view.
